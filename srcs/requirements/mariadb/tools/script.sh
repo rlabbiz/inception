9ebd07b913027b7a02446b50c7c28415e0c1e10e
Rida Labbiz <redalabbiz@gmail.com>
2024-06-02 07:36:20 -0400

    work with env variables

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
 
-export db_name='wordpress'
+export db_name="wordpress"
 export db_user='rlabbiz'
 export db_pass='0000'
 
 service mariadb start 
 
-echo "create database if not exist $(db_name);" > sql 
-echo "create user if not exist '$(db_user)'@localhost indentified by '$(db_pass)';" >> sql
-echo "grant all privileges on '$(db_name)'.* to '$(db_user)@localhost';" >> sql
+echo "CREATE DATABASE IF NOT EXISTS $$db_name;" > sql 
+echo "CREATE USER IF NOT EXISTS $db_user@'localhost' IDENTIFIED BY '$db_pass';" >> sql
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO $db_user@'localhost';" >> sql
+echo "FLUSH PRIVILEGES;" >> sql
+
 
 mysql < sql 
 
 rm sql
 
-bash
+# bash
