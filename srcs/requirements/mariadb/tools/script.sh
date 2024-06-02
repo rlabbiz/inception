288a3154280b236af9d6284f775f6942a64d12e6
Rida Labbiz <redalabbiz@gmail.com>
2024-06-02 06:38:12 -0400

    add all privileges to user

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
 export db_name='wordpress'
-export db_user='root'
+export db_user='rlabbiz'
 export db_pass='0000'
 
 service mariadb start 
 
 echo "create database if not exist $(db_name);" > sql 
-echo "create user if not exist '$(db_user)'@localhost indentified by '$(db_pass)'" >> sql
+echo "create user if not exist '$(db_user)'@localhost indentified by '$(db_pass)';" >> sql
+echo "grant all privileges on '$(db_name)'.* to '$(db_user)@localhost';" >> sql
 
 mysql < sql 
 
