8c161f7f21f1cd00cde1f37ee3f69b5e95186ab0
Rida Labbiz <redalabbiz@gmail.com>
2024-06-02 15:47:10 -0400

    work on script to create and configure the database

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
-export db_name="wordpress"
+export db_name='wordpress'
 export db_user='rlabbiz'
 export db_pass='0000'
 
 service mariadb start 
 
-echo "CREATE DATABASE IF NOT EXISTS $$db_name;" > sql 
+echo "CREATE DATABASE IF NOT EXISTS $db_name;" > sql 
 echo "CREATE USER IF NOT EXISTS $db_user@'localhost' IDENTIFIED BY '$db_pass';" >> sql
 echo "GRANT ALL PRIVILEGES ON $db_name.* TO $db_user@'localhost';" >> sql
 echo "FLUSH PRIVILEGES;" >> sql
@@ -16,4 +16,4 @@ mysql < sql
 
 rm sql
 
-# bash
+bash
