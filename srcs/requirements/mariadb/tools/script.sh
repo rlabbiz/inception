1a2e8b219bc15601fc21b1a77ad9812554bd6c32
Rida Labbiz <redalabbiz@gmail.com>
2024-06-02 06:28:53 -0400

    add env variables a

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/bash
 
+export db_name='wordpress'
+export db_user='root'
+export db_pass='0000'
+
 service mariadb start 
 
-echo "create database wordpress_db;" > sql 
+echo "create database if not exist $(db_name);" > sql 
+echo "create user if not exist '$(db_user)'@localhost indentified by '$(db_pass)'" >> sql
 
 mysql < sql 
+
+rm sql
 
 bash
