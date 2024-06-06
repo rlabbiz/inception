bd7f0574dab2df9d880d1d21b1422d4fbecf948d
Rida Labbiz <redalabbiz@gmail.com>
2024-06-06 05:24:50 -0400

    download wordpress using wp

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+wp core download wordpress --allow-root
+
 # wp config create --dbname=nginx --dbuser=rlabbiz --dbpass=0000 --path=/wordpress --skip-check --allow-root --force
 
 
