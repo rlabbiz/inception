0944260a0a99c39c555be20e9346b0fe5ddad172
Rida Labbiz <redalabbiz@gmail.com>
2024-06-06 06:23:20 -0400

    generate wp-config.php file

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,7 +2,8 @@
 
 wp core download --path=wordpress --allow-root
 
-# wp config create --dbname=nginx --dbuser=rlabbiz --dbpass=0000 --path=/wordpress --skip-check --allow-root --force
+
+wp config create --dbname=nginx --dbuser=rlabbiz --dbpass=0000 --path=/wordpress --skip-check --allow-root 
 
 
 bash
