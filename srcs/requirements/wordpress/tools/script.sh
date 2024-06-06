5dbf2f1a0eb434279f13a73e285ee939b8c68141
Rida Labbiz <redalabbiz@gmail.com>
2024-06-06 05:25:33 -0400

    fix downloading problem

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-wp core download wordpress --allow-root
+wp core download --path=wordpress --allow-root
 
 # wp config create --dbname=nginx --dbuser=rlabbiz --dbpass=0000 --path=/wordpress --skip-check --allow-root --force
 
