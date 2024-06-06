d2a433794b9a78d296695336f2caebc612e25d14
Rida Labbiz <redalabbiz@gmail.com>
2024-06-06 07:12:58 -0400

    some changes in script of wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
+export db_name=wordpress
+export db_user=rlabbiz
+export db_pass=0000
+
 wp core download --path=wordpress --allow-root
 
-
-wp config create --dbname=nginx --dbuser=rlabbiz --dbpass=0000 --path=/wordpress --skip-check --allow-root 
-
+wp config create --dbname=$db_name --dbuser=$db_user --dbpass=$db_pass --path=/wordpress --skip-check --allow-root 
 
 bash
