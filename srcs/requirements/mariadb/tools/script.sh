299cf969bfa772ae567a56ae61b6b4019bb552df
rida labbiz <redalabbiz@gmail.com>
2024-07-08 07:45:41 +0100

    update script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,18 +4,27 @@ export db_name='wordpress'
 export db_user='rlabbiz'
 export db_pass='0000'
 
-service mariadb start 
+mkdir -p /run/mysqld/ && chown -R mysql:mysql /run/mysqld/
+mkdir -p /var/lib/mysql/ && chown -R mysql:mysql /var/lib/mysql/
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name;" > sql 
-echo "CREATE USER IF NOT EXISTS $db_user@'localhost' IDENTIFIED BY '$db_pass';" >> sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO $db_user@'localhost';" >> sql
-echo "FLUSH PRIVILEGES;" >> sql
+export DB_ROOT='root'
+export DB_NAME=${db_name}
+export DB_USER=${db_user}
+export DB_PASS=${db_pass}
 
+# mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
-mysql < sql 
+cat << EOF > /tmp/create_db.sql
+CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
+GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+# run init.sql
 
-rm sql
+mysqld --user=mysql --bootstrap  < /tmp/create_db.sql
+rm -f /tmp/create_db.sql
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+# kill $(cat /var/run/mysqld/mysqld.pid)
 
 mysqld -u root
