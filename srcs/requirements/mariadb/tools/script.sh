c77c9f5e2ce1491b4be6c20d39f7848dd956a0ef
rlabbiz <redalabbiz@gmail.com>
2024-07-09 14:41:43 -0400

    design new code of mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,30 +1,16 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-export db_name='wordpress'
-export db_user='rlabbiz'
-export db_pass='0000'
+export DB_NAME='wordpress'
+export DB_USER='rlabbiz'
+export DB_PASS='0000'
 
-mkdir -p /run/mysqld/ && chown -R mysql:mysql /run/mysqld/
-mkdir -p /var/lib/mysql/ && chown -R mysql:mysql /var/lib/mysql/
-
-export DB_ROOT='root'
-export DB_NAME=${db_name}
-export DB_USER=${db_user}
-export DB_PASS=${db_pass}
-
-# mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-
-cat << EOF > /tmp/create_db.sql
-CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+echo "\
 FLUSH PRIVILEGES;
-EOF
-# run init.sql
+CREATE DATABASE IF NOT EXISTS ${DB_NAME} ;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+FLUSH PRIVILEGES;" > /sql.txt
 
-mysqld --user=mysql --bootstrap  < /tmp/create_db.sql
-rm -f /tmp/create_db.sql
+mariadbd --user=mysql --bootstrap < /sql.txt
 
-# kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld -u root
+exec "$@"
