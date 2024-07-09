7c64ea7512b79db57b66bb8175af4e2fdb43297f
rlabbiz <redalabbiz@gmail.com>
2024-07-09 14:42:04 -0400

    works on wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,13 +8,9 @@ export db_pass="0000"
 cd /var/www/html/
 wp core download --allow-root
 
-wp config create --dbname=$db_name --dbuser=$db_user --dbhost=mariadb --dbpass=$db_pass --skip-check --allow-root
-# wait for db
-while ! wp db check --allow-root
-do    
-    sleep 2
-done
+wp config create --dbname=$db_name --dbuser=$db_user --dbhost=mariadb:3306 --dbpass=$db_pass --skip-check --allow-root
+# while ! wp db check --allow-root# do    
+#     sleep 2
+# done
 
-# wp core install --url=https://mtaib.42.fr --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
-# wp user create $WP_USER $WP_USER_EMAIL --role=$ROLE --user_pass=$WP_PASS --allow-root
 exec "$@"
