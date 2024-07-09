242fb06a471a55121dbc51a622472fea41ac9a5c
rida labbiz <redalabbiz@gmail.com>
2024-07-09 09:48:49 +0100

    update script

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,20 +1,20 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-export db_name=wordpress
-export db_user=rlabbiz
-export db_pass=0000
+export db_name="wordpress"
+export db_user="rlabbiz"
+export db_pass="0000"
 
-mkdir -p /wordpress
-chown -R www-data:www-data /wordpress
-chmod -R 755 /wordpress
 
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+cd /var/www/html/
+wp core download --allow-root
 
-chmod +x wp-cli.phar 
-mv wp-cli.phar /usr/local/bin/wp
+wp config create --dbname=$db_name --dbuser=$db_user --dbhost=mariadb --dbpass=$db_pass --skip-check --allow-root
+# wait for db
+while ! wp db check --allow-root
+do    
+    sleep 2
+done
 
-wp core download --path=wordpress --allow-root
-
-wp config create --dbname=$db_name --dbuser=$db_user --dbpass=$db_pass --path=/wordpress --skip-check --allow-root 
-
-bash
+# wp core install --url=https://mtaib.42.fr --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
+# wp user create $WP_USER $WP_USER_EMAIL --role=$ROLE --user_pass=$WP_PASS --allow-root
+exec "$@"
