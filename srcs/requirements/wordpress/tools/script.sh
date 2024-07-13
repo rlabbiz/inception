93cfd51e114b2c84ddc01318c02b8aea4658d3f1
rida labbiz <redalabbiz@gmail.com>
2024-07-13 08:42:05 +0100

    add new user

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-export db_name="wordpress"
-export db_user="rlabbiz"
-export db_pass="0000"
-
-
 cd /var/www/html/
+
 wp core download --allow-root
 
-wp config create --dbname=$db_name --dbuser=$db_user --dbhost=mariadb:3306 --dbpass=$db_pass --skip-check --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbhost=mariadb:3306 --dbpass=$DB_PASS --skip-check --allow-root
 
-wp core install --allow-root --url=rlabbiz.42.fr --title=rlabbiz --admin_user=rlabbiz --admin_password=0000 --admin_email=redalabbiz@gmail.com
+wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL
 
-wp user create	--allow-root reda reda@gmail.com --role=author --user_pass=0000
+wp user create	--allow-root $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS
 
 exec "$@"
