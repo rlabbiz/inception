a1a393485baff9bd32d5f132e803ee8691ff1cb1
rlabbiz <redalabbiz@gmail.com>
2024-07-14 15:15:47 -0400

    push

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,6 +2,8 @@
 
 cd /var/www/html/
 
+if [ ! -f /var/www/html/wp-config.php ]; then
+
 wp core download --allow-root
 
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbhost=mariadb:3306 --dbpass=$DB_PASS --skip-check --allow-root
@@ -9,5 +11,7 @@ wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbhost=mariadb:3306 --dbp
 wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL
 
 wp user create	--allow-root $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS
+
+fi
 
 exec "$@"
